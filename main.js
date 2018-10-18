5d62686ef2d43b64f12afa2b12e224024fb7c0d8
Patrick Neumüller <patrick.neumueller@hs-onsabrueck.de>
2018-10-18 22:32:49 +0200

    prototype.spawn

--- main.js
+++ main.js
@@ -1,69 +1,71 @@
+// import modules
+require('prototype.spawn')();
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
-var require('prototype.spawn');
 
 module.exports.loop = function () {
-    //Memory loeschen
-    for(let name in Memory.creeps){
-        if(Game.creeps[name] == undefined){
+
+    for (let name in Memory.creeps) {
+        if (Game.creeps[name] == undefined) {
             delete Memory.creeps[name];
         }
     }
 
-
     for (let name in Game.creeps) {
         var creep = Game.creeps[name];
 
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        else if(creep.memory.role == 'upgrader') {
+        else if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
-        else if(creep.memory.role == 'builder'){
+        else if (creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
-        else if(creep.memory.role == 'repairer'){
+        else if (creep.memory.role == 'repairer') {
             roleRepairer.run(creep);
         }
     }
 
-    var minimumNumberOfHarvester = 10;
+    var minimumNumberOfHarvesters = 4;
     var minimumNumberOfUpgraders = 1;
     var minimumNumberOfBuilders = 1;
     var minimumNumberOfRepairers = 2;
-    
+
     var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
     var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
     var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
     var numberOfRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
 
+    var energy = Game.spawns.Spawn1.room.energyCapacityAvailable;
     var name = undefined;
 
-    if(numberOfHarvesters < minimumNumberOfHarvester) {
-        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE],undefined,
-            { role:'harvester', working: false });
+    if (numberOfHarvesters < minimumNumberOfHarvesters) {
+        name = Game.spawns.Spawn1.createCustomCreep(energy, 'harvester');
+
+        if (name == ERR_NOT_ENOUGH_ENERGY && numberOfHarvesters == 0) {
+            name = Game.spawns.Spawn1.createCustomCreep(
+                Game.spawns.Spawn1.room.energyAvailable, 'harvester');
+        }
     }
-    else if ( numberOfUpgraders < minimumNumberOfUpgraders) {
-        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,CARRY,MOVE,MOVE],undefined,
-            { role:'upgrader', working: false });
+    else if (numberOfUpgraders < minimumNumberOfUpgraders) {
+        name = Game.spawns.Spawn1.createCustomCreep(energy, 'upgrader');
     }
-    else if ( numberOfRepairers < minimumNumberOfRepairers) {
-        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE],undefined,
-            { role:'repairer', working: false });
+    else if (numberOfRepairers < minimumNumberOfRepairers) {
+        name = Game.spawns.Spawn1.createCustomCreep(energy, 'repairer');
     }
     else if (numberOfBuilders < minimumNumberOfBuilders) {
-        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE],undefined,
-            { role:'builder', working: false });
+        name = Game.spawns.Spawn1.createCustomCreep(energy, 'builder');
     }
     else {
-        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE],undefined,
-            { role:'repairer', working: false });
+        name = Game.spawns.Spawn1.createCustomCreep(energy, 'builder');
     }
 
-    if(!(name < 0)){
-        console.log("Spanwn new creep: " + name + " || Role: "+ Game.creeps[name].memory.role);
+
+    if (!(name < 0)) {
+        console.log("Spawned new creep: " + name + " || Role: " + Game.creeps[name].memory.role);
     }
 };
--- role.harvester.js
+++ role.harvester.js
@@ -1,27 +1,41 @@
 module.exports = {
-    run: function(creep){
-        
-         if (creep.memory.working == true && creep.carry.energy == 0) {
+
+    run: function(creep) {
+
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+
             creep.memory.working = false;
         }
+
         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+
             creep.memory.working = true;
         }
 
-    
+
         if (creep.memory.working == true) {
+
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                filter: (s) => s.energy < s.energyCapacity
+
+                filter: (s) => (s.structureType == STRUCTURE_SPAWN
+                             || s.structureType == STRUCTURE_EXTENSION)
+                             && s.energy < s.energyCapacity
             });
 
+
             if (structure != undefined) {
+
                 if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+
                     creep.moveTo(structure);
                 }
             }
         }
+
         else {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+
+            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
 
                 creep.moveTo(source);
