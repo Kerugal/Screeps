d8b09b10820c6e3dfc069597e271ad2a592055f3
Patrick Neumüller <patrick.neumueller@hs-onsabrueck.de>
2018-10-19 20:39:49 +0200

    longdistance

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 var roleWallRepairer = require('role.wallRepairer');
+var roleLongDistanceHarvester = require('role.longDistanceHarvester');
+
+var HOME = 'W8N3';
 
 module.exports.loop = function () {
 
@@ -32,30 +35,38 @@ module.exports.loop = function () {
         else if (creep.memory.role == 'wallRepairer') {
             roleWallRepairer.run(creep);
         }
+        else if (creep.memory.role == 'longDistanceHarvester') {
+            roleLongDistanceHarvester.run(creep);
+        }
     }
 
-    var towers = Game.rooms.W5N3.find(FIND_STRUCTURES, {
+    var towers = Game.rooms[HOME].find(FIND_STRUCTURES, {
         filter: (s) => s.structureType == STRUCTURE_TOWER
     });
-
-    for (let tower of towers){
+    for (let tower of towers) {
         var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (target != undefined){
+        if (target != undefined) {
             tower.attack(target);
         }
     }
 
-    var minimumNumberOfHarvesters = 5;
-    var minimumNumberOfUpgraders = 2;
+    var minimumNumberOfHarvesters = 1;
+    var minimumNumberOfUpgraders = 1;
     var minimumNumberOfBuilders = 1;
-    var minimumNumberOfRepairers = 2;
+    var minimumNumberOfRepairers = 1;
     var minimumNumberOfWallRepairers = 1;
+    var minimumNumberOfLongDistanceHarvestersW3N5 = 1;
+    var minimumNumberOfLongDistanceHarvestersW2N4 = 1;
 
     var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
     var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
     var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
     var numberOfRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
     var numberOfWallRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'wallRepairer');
+    var numberOfLongDistanceHarvestersW3N5 = _.sum(Game.creeps, (c) =>
+        c.memory.role == 'longDistanceHarvester' && c.memory.target == 'W8N2');
+    var numberOfLongDistanceHarvestersW2N4 = _.sum(Game.creeps, (c) =>
+        c.memory.role == 'longDistanceHarvester' && c.memory.target == 'W7N3');
 
     var energy = Game.spawns.Spawn1.room.energyCapacityAvailable;
     var name = undefined;
@@ -80,10 +91,15 @@ module.exports.loop = function () {
     else if (numberOfWallRepairers < minimumNumberOfWallRepairers) {
         name = Game.spawns.Spawn1.createCustomCreep(energy, 'wallRepairer');
     }
+    else if (numberOfLongDistanceHarvestersW3N5 < minimumNumberOfLongDistanceHarvestersW3N5) {
+        name = Game.spawns.Spawn1.createLongDistanceHarvester(energy, 5, HOME, 'W8N2', 0);
+    }
+    else if (numberOfLongDistanceHarvestersW2N4 < minimumNumberOfLongDistanceHarvestersW2N4) {
+        name = Game.spawns.Spawn1.createLongDistanceHarvester(energy, 3, HOME, 'W7N3', 0);
+    }
     else {
         name = Game.spawns.Spawn1.createCustomCreep(energy, 'builder');
     }
-
 
     if (!(name < 0)) {
         console.log("Spawned new creep: " + name + " || Role: " + Game.creeps[name].memory.role);
--- role.repairer.js
+++ role.repairer.js
@@ -3,17 +3,12 @@ var roleBuilder = require('role.builder');
 module.exports = {
 
     run: function(creep) {
-
         if (creep.memory.working == true && creep.carry.energy == 0) {
-            // switch state
             creep.memory.working = false;
         }
-
         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-
             creep.memory.working = true;
         }
-
 
         if (creep.memory.working == true) {
 
@@ -21,26 +16,19 @@ module.exports = {
                 filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL
             });
 
+            // if we find one
             if (structure != undefined) {
-
                 if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
-
                     creep.moveTo(structure);
                 }
             }
-
             else {
-
                 roleBuilder.run(creep);
             }
         }
-
         else {
-
             var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-
                 creep.moveTo(source);
             }
         }
